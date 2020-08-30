df631dc2373d63d945613e77d34f62aef94165d2
Alex34fr <alexandre.reynaud1@gmail.com>
2020-08-31 01:36:29 +0900

    fixed selected month format

--- ce_monthly.sh
+++ ce_monthly.sh
@@ -30,7 +30,7 @@ echo " "
 echo -e "${MAGENTA} 2. Choose the month you want to check for $select_prof :${NC}\n"
 
 echo " "
-echo "================== 2020 =================="
+echo "================== 2020 ================="
 echo " "
 echo " 01 : January	 	  07 : July"		
 echo " 02 : February		  08 : August"	
@@ -39,44 +39,66 @@ echo " 04 : April		  10 : October"
 echo " 05 : May 		  11 : November"
 echo " 06 : June		  12 : December"
 echo " "
-read -p '-->  Enter the MONTH for which you want the billing( 01, 02...) ' month
-
-
-if [[ "$month" = "01" ]] ; then
-  MON_STR=January
-elif [[ "$month" = "02" ]] ; then
-  MON_STR=February
-elif [[ "$month" = "03" ]] ; then
-  MON_STR=March
-elif [[ "$month" = "04" ]] ; then
-  MON_STR=April
-elif [[ "$month" = "05" ]] ; then
-  MON_STR=May
-elif [[ "$month" = "06" ]] ; then
-  MON_STR=June
-elif [[ "$month" = "07" ]] ; then
-  MON_STR=July
-elif [[ "$month" = "08" ]] ; then
-  MON_STR=August
-elif [[ "$month" = "09" ]] ; then
-  MON_STR=September
-elif [[ "$month" = "10" ]] ; then
-  MON_STR=October
-elif [[ "$month" = "11" ]] ; then
-  MON_STR=November
-elif [[ "$month" = "12" ]] ; then
-  MON_STR=December
-fi
-
-PERIOD='Start=2020-'$month'-01,End=2020-'0$(($month + 1))'-01'
-
-billing=$(aws --profile $select_prof ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
-
-printf "\nBilling of ${green}$select_prof ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"
 
 
 else
-		echo "Profile not found"
+echo -e "Profile not found"
+exit 0
 
 fi
+
+#  read -p "Enter a number between 2 and 5: " number
+read -p '-->  Enter the MONTH for which you want the billing(1,2,3...): ' month
+  
+#format 'month' into 2 decimal number
+monthd=$(printf %02d $month)
+#nxtmonth=$(($monthd + 1))
+  
+ if (($monthd >= 01 && $monthd <= 12)); then
+
+
+if [[ "$monthd" = "01" ]] ; then
+  MON_STR=January
+elif [[ "$monthd" = "02" ]] ; then
+  MON_STR=February
+elif [[ "$monthd" = "03" ]] ; then
+  MON_STR=March
+elif [[ "$monthd" = "04" ]] ; then
+  MON_STR=April
+elif [[ "$monthd" = "05" ]] ; then
+  MON_STR=May
+elif [[ "$monthd" = "06" ]] ; then
+  MON_STR=June
+elif [[ "$monthd" = "07" ]] ; then
+  MON_STR=July
+elif [[ "$monthd" = "08" ]] ; then
+  MON_STR=August
+elif [[ "$monthd" = "09" ]] ; then
+  MON_STR=September
+elif [[ "$monthd" = "10" ]] ; then
+  MON_STR=October
+elif [[ "$monthd" = "11" ]] ; then
+  MON_STR=November
+elif [[ "$monthd" = "12" ]] ; then
+  MON_STR=December
+fi
+
+else
+echo -e "${WARNING}Enter a valid month${NC}\n"
+exit 0
+
+fi
+
+#2020
+PERIOD='Start=2020-'$monthd-01',End=2020-'$(printf %02d $(($monthd + 1)))'-01'
+#2019
+#PERIOD='Start=2019-'$monthd-01',End=2019-'$(printf %02d $(($monthd + 1)))'-01'
+
+billing=$(aws --profile $select_prof ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
+
+
+
+printf "\nBilling of ${green}$select_prof ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"  
+
+
 
