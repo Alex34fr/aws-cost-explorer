a1e833d2972db1e1a60023623d124303fd0bf14f
Alex34fr <alexandre.reynaud1@gmail.com>
2020-08-30 19:58:42 +0900

    changes on dates and colors

--- ce_monthly.sh
+++ ce_monthly.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 #@lex2020
 
-RED='\033[0;31m'
+blue=$(tput setaf 4)
+normal=$(tput sgr0)
 CYAN='\033[96m'
 MAGENTA='\033[95m'
 GREY='\033[90m'
 OKBLUE='\033[94m'
 OKGREEN='\033[92m'
 WARNING='\033[93m'
-FAIL='\033[91m'
 NC='\033[0m'
 
-echo -e "${MAGENTA}Choose the AWS profile you want to check :${NC}\n"
 
-echo "Your AWS Profiles List configured in your machine : "
+
 echo ""
+echo -e "${MAGENTA} 1. Type the AWS profile you want to check :${NC}\n"
+
+echo -e "${OKGREEN} Your AWS profiles configured in your machine are: ${NC}\n"
 aws configure list-profiles
 echo "For All: type all"
 echo ""
@@ -23,7 +25,9 @@ read -p '--> YOUR CHOICE : ' select_prof
 
 if [ -n $select_prof ] ; then
 
-#read -p '-->  Enter profile you want to check: ' profile
+echo " "
+echo -e "${MAGENTA} 2. Choose the month you want to check for $select_prof :${NC}\n"
+
 echo " "
 echo "================== 2020 =================="
 echo " "
@@ -34,14 +38,41 @@ echo " 04 : April		  10 : October"
 echo " 05 : May 		  11 : November"
 echo " 06 : June		  12 : December"
 echo " "
-read -p '-->  Enter the month for which you want the billing( 01, 02...) ' month
+read -p '-->  Enter the MONTH for which you want the billing( 01, 02...) ' month
+
+
+if [[ "$month" = "01" ]] ; then
+  MON_STR=January
+elif [[ "$MON_STR" = "Feb" ]] ; then
+  MON_NUM=02
+elif [[ "$MON_STR" = "Mar" ]] ; then
+  MON_NUM=03
+elif [[ "$MON_STR" = "Apr" ]] ; then
+  MON_NUM=04
+elif [[ "$MON_STR" = "May" ]] ; then
+  MON_NUM=05
+elif [[ "$MON_STR" = "Jun" ]] ; then
+  MON_NUM=06
+elif [[ "$MON_STR" = "Jul" ]] ; then
+  MON_NUM=07
+elif [[ "$MON_STR" = "Aug" ]] ; then
+  MON_NUM=08
+elif [[ "$MON_STR" = "Sep" ]] ; then
+  MON_NUM=09
+elif [[ "$MON_STR" = "Oct" ]] ; then
+  MON_NUM=10
+elif [[ "$MON_STR" = "Nov" ]] ; then
+  MON_NUM=11
+elif [[ "$MON_STR" = "Dec" ]] ; then
+  MON_NUM=12
+fi
 
 
 PERIOD='Start=2020-'$month'-01,End=2020-'0$(($month + 1))'-01'
 
 billing=$(aws --profile $select_prof ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
 
-printf "Billing of $select_prof for $month:  %s\n " "$(printf "%8.2f\n" "$billing")$ "
+printf "Billing of $select_prof for $MON_STR 2020 =%s\n " "${blue}$(printf "%8.2f\n" "$billing")$ ${normal}"
 
 else
 		echo "Profile not found"
