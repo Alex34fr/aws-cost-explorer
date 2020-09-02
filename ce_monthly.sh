39317c5666199ae4d6c9f93f807ba31ff1b0ca4b
Alex34fr <alexandre.reynaud1@gmail.com>
2020-09-02 20:28:34 +0900

    change list-profiles management

--- ce_monthly.sh
+++ ce_monthly.sh
@@ -18,49 +18,53 @@ echo ""
 echo -e "${MAGENTA} 1. Type the AWS profile you want to check :${NC}\n"
 
 echo -e "${OKGREEN} Your AWS profiles configured in your machine are: ${NC}\n"
-aws configure list-profiles
-#echo "For All: type all"
+
+#display your profiles list:
+aws configure list-profiles > profil_list.txt
+
+prof_list='profil_list.txt'
+n=1
+while read line; do
+# reading each line
+echo "$n. $line"
+n=$((n+1))
+done < $prof_list
+
+#echo "> For All: type all"
 echo ""
 
 read -p '--> YOUR CHOICE : ' select_prof
 
-if [ -n $select_prof ] ; then
+if [ -z "$select_prof" ] ; then
+echo -e "${WARNING}Profile not selected${NC}\n"
+exit 0
 
+else
+
+profile=$(head -"$select_prof" $prof_list | tail -1)
+
+# choose your month:
 echo " "
 echo -e "${MAGENTA} 2. Choose the month you want to check for $select_prof :${NC}\n"
 
 echo " "
 echo "================== 2020 ================="
 echo " "
-echo " 01 : January	 	  07 : July"		
-echo " 02 : February		  08 : August"	
-echo " 03 : March		  09 : September"
-echo " 04 : April		  10 : October"
-echo " 05 : May 		  11 : November"
-echo " 06 : June		  12 : December"
+echo " 1 : January	 	  7 : July"		
+echo " 2 : February		  8 : August"	
+echo " 3 : March		  9 : September"
+echo " 4 : April		  10 : October"
+echo " 5 : May 		  11 : November"
+echo " 6 : June		  12 : December"
 echo " "
 
+read -p '-->  Enter the MONTH for which you want the billing (1,2,3...11,12): ' month
 
-else
-echo -e "Profile not found"
-exit 0
-
-fi
-
-read -p '-->  Enter the MONTH for which you want the billing (1,2,3...): ' month
-  
 ##format 'month' into 2 decimal number
-
-
 #monthd=$(printf %02d $month)
-
-
-##nxtmonth=$(($monthd + 1))
-  
-echo $monthd
-
-
-  #if (($monthd >= 1 && $monthd <= 12)); then
+##nxtmonth=$(($monthd + 1)) 
+#echo $monthd
+#if (($monthd >= 1 && $monthd <= 12)); then
 
 
 if [[ "$month" = "1" ]] ; then
@@ -93,20 +97,20 @@ fi
 #echo -e "${WARNING}Enter a valid month${NC}\n"
 #exit 0
 
-#fi
+fi
 
 #2020
 PERIOD='Start=2020-0'$month'-01,End=2020-'0$(($month + 1))'-01'
 #2019
 #PERIOD='Start=2019-'$monthd-01',End=2019-'$(printf %02d $(($monthd + 1)))'-01'
 
-billing=$(aws --profile $select_prof ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
+billing=$(aws --profile $profile ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
 
 
 
-printf "\nBilling of ${green}$select_prof ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"  
+printf "\nBilling of ${green}$profile ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"  
 
-echo "$select_prof |$MON_STR 2020|$(printf "%8.2f" "$billing")$ " >> $out_path
+echo "$profile |$MON_STR 2020|$(printf "%8.2f" "$billing")$ " >> $out_path
 
 exit 0
 
