b2a4a6c37a3d3ee9f21d2843b37c532b18eae295
Alex34fr <alexandre.reynaud1@gmail.com>
2020-09-04 00:05:30 +0900

    change for ALL profiles conditions

--- ce_monthly.sh
+++ ce_monthly.sh
@@ -11,32 +11,35 @@ MAGENTA='\033[95m'
 OKGREEN='\033[92m'
 ERROR='\033[9m'
 NC='\033[0m'
+path_output=/Users/$(printenv USER)/Downloads
 
+#choose the profil:
 
 echo ""
 echo -e "${MAGENTA} 1. Type the AWS profile you want to check :${NC}\n"
-echo -e "${OKGREEN} Your AWS profiles configured in your machine are: ${NC}\n"
+echo -e "${OKGREEN} AWS profiles configured in your machine are: ${NC}\n"
 
 #display your profiles list:
 aws configure list-profiles > profil_list.txt
 prof_list='profil_list.txt'
+nbline=$(cat profil_list.txt | wc -l)
+
 n=1
 while read line; do
-# reading each line
+#reading each line
 echo "$n. $line"
 n=$((n+1))
 done < $prof_list
-echo "For All: type 'all'"
+echo "ALL.To select all profiles: type 'all'"
 echo ""
 
-read -p '--> YOUR CHOICE : ' select_prof
-if [ -z "$select_prof" ] ; then
-printf "${red} Profile not selected ${NC}\n"
-exit 0
+read -p '==> YOUR CHOICE : ' select_prof
 
-else
+#check if selected profil exists or selection is "all" :
 
-# choose your month:
+ if [[ "$select_prof" -ge 1 && "$select_prof" -le "$nbline" ]] || [ "$select_prof" = "all" ] ; then
+
+# choose the month:
 echo " "
 echo -e "${MAGENTA} 2. Choose the month you want to check for $select_prof :${NC}\n"
 
@@ -51,7 +54,8 @@ echo " 5 : May            11 : November"
 echo " 6 : June           12 : December"
 echo " "
 
-read -p '-->  Enter the MONTH for which you want the billing (1,2,3...11,12): ' month
+read -p '==>   Enter the MONTH for which you want the billing (1,2,3...11,12): ' month
+
 
 if [[ "$month" = "1" ]] ; then
   MON_STR=January
@@ -78,32 +82,45 @@ elif [[ "$month" = "11" ]] ; then
 elif [[ "$month" = "12" ]] ; then
   MON_STR=December
 fi
+
+else
+
+printf "${red} Wrong selection, please try again...${NC}\n"
+exit 0
+
 fi
 
 # all profiles case
 if [[ "$select_prof" = "all" ]] ; then
+echo " 
+===============================================================
+        Billing for all AWS accounts on $MON_STR 2020
+==============================================================="
+> $path_output/AWS_billing_all_${MON_STR}_2020.txt
 n=1
 while read line; do
 # reading each line of profiles_list
 profile=$(head -$n $prof_list | tail -1)
 PERIOD='Start=2020-0'$month'-01,End=2020-'0$(($month + 1))'-01'
 billing=$(aws --profile $profile ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
-echo "$n. $profile for $MON_STR 2020 = $(printf "%8.2f" "$billing") $" >> Billing_all_${MON_STR}_2020.txt
-printf "\nBilling of ${green}$profile ${normal}for ${green}$MON_STR 2020${normal} =%s\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"
+
+echo "$n. $profile for $MON_STR 2020 = $(printf "%8.2f" "$billing") $" >> $path_output/AWS_billing_all_${MON_STR}_2020.txt
+
+printf "\n$n. ${green}$profile ${normal}for ${green}$MON_STR 2020${normal} =%s\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"
 n=$((n+1)) 
 done < $prof_list
 
 else
 
-#single case
+#single profile case
 profile=$(head -"$select_prof" $prof_list | tail -1)
 #2020
 PERIOD='Start=2020-0'$month'-01,End=2020-'0$(($month + 1))'-01'
 #2019
-#PERIOD='Start=2019-'$monthd-01',End=2019-'$(printf %02d $(($monthd + 1)))'-01'
+#PERIOD='Start=2019-0'$month'-01,End=2019-'0$(($month + 1))'-01'
 
 billing=$(aws --profile $profile ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
-printf "\nBilling of ${green}$profile ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"
+printf "\nBilling of ${blue}$profile ${normal}for ${green}$MON_STR 2020${normal} =%s\n\n " "${blue}$(printf "%8.2f" "$billing")$ ${normal}"
 
 fi
 exit 0
