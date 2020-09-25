ff7fc440f5a177e94b92f797c4ec3204aba57a8e
Alex34fr <alexandre.reynaud1@gmail.com>
2020-09-25 12:00:14 +0900

    add : month and year input conditions

--- ce_monthly.sh
+++ ce_monthly.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #@lex2020
+#Get the AWS monthly cost for one (or all) of your configured profile(s) 
 
 #Variables
 red=$(tput setaf 1)
@@ -11,7 +12,6 @@ OKGREEN='\033[92m'
 ERROR='\033[9m'
 NC='\033[0m'
 path_output=/Users/$(printenv USER)/Downloads
-
 
 # choose the profil:
 
@@ -35,15 +35,17 @@ echo ""
 
 read -p '==> Enter the profile number of your choice (1,2,...,ALL): ' select_prof
 
-# check if selected profil exists or selection is "all" :
+# check if selected profil exists or selection is "all":
 
- if [[ "$select_prof" -ge 1 && "$select_prof" -le "$nbline" ]] || [[ "$select_prof" = "all" || "$select_prof" = "ALL" ]] ; then
+if [[ "$select_prof" -ge 1 && "$select_prof" -le "$nbline" ]] || [[ "$select_prof" = [aA]ll || "$select_prof" = "ALL" ]] ; then
 
 # choose the year:
 
 echo " "
 echo -e "${MAGENTA} 2. Choose the YEAR for which you want to check the billing :${NC}\n"
 read -p '==>   Enter the YEAR for which you want the billing (2019,2020...): ' year
+
+if [[ -n "$year" ]] && [[ "$year" = 2019 || "$year" = 2020 ]] ; then
 
 # choose the month:
 echo " "
@@ -62,9 +64,14 @@ echo " "
 
 read -p '==>   Enter the MONTH for which you want the billing (1,2,3...11,12): ' month
 
-# check if selected profil exists or selection is "all" :
+else
+printf "${red} Wrong year input, please try again...${NC}\n"
+exit 0
+fi
 
- if [[ "$month" -ge 1 && "$month" -le 12 ]] ; then
+# check if selected profil exists or selection is "all":
+
+if [[ "${#month}" -le 2 ]] && [[ "$month" -ge 1 && "$month" -le 12 ]] ; then
 
 monthd=$(printf %02d $month)
 
@@ -97,38 +104,38 @@ fi
 else
 printf "${red} Wrong month selection, please try again...${NC}\n"
 exit 0
-
 fi 
 
 else
 
 printf "${red} Wrong profile selection, please try again...${NC}\n"
 exit 0
-
 fi
 
+# case for December:
 if [[ "$monthd" = "12" ]] ; then
 nxtmonth=01
 endyear=$(( $year + 1 ))
+
 else
 nxtmonth=$(printf %02d $(( $month + 1 )))
 monthd=$(printf %02d $month)
 endyear=$year
 fi
 
-# all profiles case
+# 'all profiles' case:
 
-if [[ "$select_prof" = "all" || "$select_prof" = "ALL" ]] ; then
-
+if [[ "$select_prof" = [aA]ll || "$select_prof" = "ALL" ]] ; then
 
 echo " 
 ===============================================================
         Billing for all AWS accounts on $MON_STR $year
 ==============================================================="
 > $path_output/AWS_billing_all_${MON_STR}_${year}.txt
+
+ # reading each line of profiles_list:
 n=1
 while read line; do
-  # reading each line of profiles_list
 profile=$(head -$n $prof_list | tail -1)
 PERIOD='Start='$year'-'$monthd'-01,End='$endyear'-'$nxtmonth'-01'
 
@@ -136,11 +143,12 @@ bloc_profile=$(grep -i -A 3 "profile "$(head -$((n+1)) $prof_list | tail -1)"" ~
 
 printf "\n$n. ${blue}$profile : ${normal}\n "
 
+# get monthly cost of each profile:
 billing=$(aws --profile $profile ce get-cost-and-usage --time-period $PERIOD --granularity MONTHLY --metrics "BlendedCost" --output text |awk 'FNR == 3 {print $2}')
 
 echo "$n. $profile for $MON_STR $year = $(printf "$%.2f\n" "$billing")" >> $path_output/AWS_billing_all_${MON_STR}_${year}.txt
 
- # check profile using MFA
+# check if profile using MFA:
 if  echo "$bloc_profile" | grep -q "mfa_serial"  ; then
 
 printf "\n Bill for ${green}$profile ${normal}in ${green}$MON_STR $year${normal} = %s\n " "${blue}$(printf "$%.2f" "$billing") ${normal}"
@@ -148,8 +156,7 @@ printf "\n Bill for ${green}$profile ${normal}in ${green}$MON_STR $year${normal}
 printf "\n ${red}>> Next profile ($(head -$((n+1)) $prof_list | tail -1)) uses MFA so please wait 30s for a new MFA code on your Authenticator Device...${normal}
      
      "
-#source ./progress-bar.sh
-#progress-bar 30
+sleep 30
 
 else
 printf "\n Bill for ${green}$profile ${normal}in ${green}$MON_STR $year${normal} = %s\n " "${blue}$(printf "$%.2f" "$billing") ${normal}"
@@ -162,10 +169,11 @@ printf "\n${MAGENTA}*** INFO ***    GET-COST for All your accounts ended success
 *** INFO ***    You can find your bills summarize in ${red}$path_output/AWS_billing_all_${MON_STR}_${year}.txt\n ${normal}
 
 "
+# clear your aws cache:
 rm -r ~/.aws/cli/cache
 else
 
- #single profile case
+# single profile case:
 
 profile=$(head -"$select_prof" $prof_list | tail -1)
 
